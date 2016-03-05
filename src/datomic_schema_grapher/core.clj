86b8d356d1f48be7934d57dc7ed0dab57c1f8681
Leon Grapenthin <grapenthin.leon@gmail.com>
2016-03-05 23:59:49 +0100

    can pass db to graph-datomic

--- src/datomic_schema_grapher/core.clj
+++ src/datomic_schema_grapher/core.clj
@@ -4,11 +4,9 @@
             [datomic-schema-grapher.dot :refer (show to-dot)])
   (:import javax.swing.JFrame))
 
-(defn graph-datomic
-  "Render a nice graphviz of your Datomic schema"
-  [uri & {:keys [save-as no-display exit-on-close]}]
-  (let [db (d/db (d/connect uri))
-        schema (schema db)
+(defn graph-datomic-db
+  [db & {:keys [save-as no-display exit-on-close]}]
+  (let [schema (schema db)
         references (references db)]
     (if save-as
       (spit save-as (to-dot schema references)))
@@ -18,3 +16,8 @@
           ;; mainly for use with the lein plugin
           (.setDefaultCloseOperation jframe JFrame/EXIT_ON_CLOSE)
           (while true (Thread/sleep 500)))))))
+
+(defn graph-datomic
+  "Render a nice graphviz of your Datomic schema"
+  [uri & {:keys [save-as no-display exit-on-close]}]
+  (graph-datomic-db (d/db (d/connect uri))))
