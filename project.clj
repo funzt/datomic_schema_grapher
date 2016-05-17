e12ed89c3c52b15ed7af50f97a884956f30f9fa3
Leon Grapenthin <grapenthin.leon@gmail.com>
2016-05-17 09:12:54 +0200

    make it a funzt.jetzt open source project

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject datomic-schema-grapher "0.0.1"
+(defproject org.funzt/datomic-schema-grapher "0.1.0-SNAPSHOT"
   :description "A library and lein plugin for graphing the datomic schema."
-  :url "https://github.com/felixflores/datomic_schema_grapher"
-  :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [dorothy "0.0.6"]
                  [hiccup "1.0.5"]]
